32765e563686ac049c240cae888529fb4732b560
Hassan Amir <hassanamirgp@gmail.com>
2026-07-11 11:24:05 +0500

    Added exerice 1.6

---
+++ part1/feedback/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Button = props => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text='Good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text='Neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text='Bad'/>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
